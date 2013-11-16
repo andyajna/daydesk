08f001466970a2ffacd7e8f156e9328c44bb640b
Andrew Kemp <andrewkemp70@gmail.com>
2013-11-16 19:40:28 +1100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Daydesk::Application.config.secret_key_base = 'e493b34d604e30e75df1858b8532a93ba0b984af310a46376477130e5c40efc73e7eb969eb1d099ad8851aab5b44b7876995dd5c55c75238307021524dd9f343'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Daydesk::Application.config.session_store :cookie_store, key: '_daydesk_session'
